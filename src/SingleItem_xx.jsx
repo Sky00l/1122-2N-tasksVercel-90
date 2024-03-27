7b9c969ffb2bd3bdd9a48993e26b9f6771c1e311
Sky00l <207410290@gms.tku.edu.tw>
2024-03-27 19:57:07 +0800

    upload

--- src/SingleItem_xx.jsx
+++ src/SingleItem_xx.jsx
@@ -1,10 +1,53 @@
-const SingleItem = ({ item }) => {
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { supabase } from './db/clientSupabase'
+import { toast } from 'react-toastify'
+
+const SingleItem_90 = ({ item }) => {
+  const queryClient = useQueryClient()
+  const { mutate: editTask } = useMutation({
+    mutationFn: async ({ taskId, isDone }) => {
+      try {
+        const { data, error } = await supabase
+          .from('task_90')
+          .update({ is_done: isDone })
+          .eq('id', taskId)
+          .select()
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks']
+      })
+      toast.sussess('task updated')
+    }
+  })
+
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: async taskId => {
+      try {
+        const { error } = await supabase
+          .from('task_90')
+          .delete()
+          .eq('id', taskId)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks']
+      })
+      toast.sussess('task deleted')
+    }
+  })
   return (
     <div className='single-item'>
       <input
         type='checkbox'
-        checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        checked={item.is_Done}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.is_done })}
       />
       <p
         style={{
@@ -17,11 +60,11 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
     </div>
   )
 }
-export default SingleItem
+export default SingleItem_90
