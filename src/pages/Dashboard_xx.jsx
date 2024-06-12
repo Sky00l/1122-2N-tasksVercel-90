19ad60b0393c700cc9df742eb2a619aa52d2a9af
sky00l <207410290@gms.tku.edu.tw>
2024-06-12 19:36:32 +0800

    modified Dashboard_xx.jsx

--- src/pages/Dashboard_xx.jsx
+++ src/pages/Dashboard_xx.jsx
@@ -1,5 +1,35 @@
-const Dashboard_xx = () => {
-  return <>Dashboard_xx page</>;
-};
+import styled from 'styled-components';
+
+const Layout = styled.main`
+  display: grid;
+  grid-template-columns: 48rem;
+  align-content: center;
+  justify-content: center;
+  gap: 3.2rem;
+  background-color: var(--color-grey-50);
+  list-style-type: none;
+  h1 {
+    font-size: 18px;
+    text-align: center;
+    font-weight: bolder;
+  }
+`;
+function Dashboard_xx() {
+  return (
+    <Layout>
+      <div>
+        <h1>Final 2 Exam of Web Prog2, 2024</h1>
+      </div>
+      <div>
+        <h2>
+          <li>Guest name:林泓君</li>
+          <li>Guest student ID:207410290</li>
+          <li>Supabase email:207410290@gmail.com</li>
+          <li>Supabase user ID:9e30df4d-6968-46cf-a745-810d8719319b</li>
+        </h2>
+      </div>
+    </Layout>
+  );
+}
 
 export default Dashboard_xx;
